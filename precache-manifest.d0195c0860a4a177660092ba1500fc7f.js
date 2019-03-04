5c4a43e90f0da67f743db06a6c1d22f2e97fc142
Thomi Jasir <thomijasir@gmail.com>
2019-03-04 21:03:58 +0700

    Updates

---
+++ precache-manifest.d0195c0860a4a177660092ba1500fc7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "722a41b0c318a76c64f0",
+    "url": "/static/js/main.722a41b0.chunk.js"
+  },
+  {
+    "revision": "e9ba764aa203261740cf",
+    "url": "/static/js/2.e9ba764a.chunk.js"
+  },
+  {
+    "revision": "722a41b0c318a76c64f0",
+    "url": "/static/css/main.796c29c0.chunk.css"
+  },
+  {
+    "revision": "e9ba764aa203261740cf",
+    "url": "/static/css/2.284e50ba.chunk.css"
+  },
+  {
+    "revision": "0a27c61a5331e2dfec76917e1d25bb83",
+    "url": "/index.html"
+  }
+];
